497d6c30c83e0a6a11948513b6e1ab1fc56d23d6
Andrwyl <andrewyl438@gmail.com>
2024-03-21 18:27:49 -0400

    added freezing after collision and finished trivia logic
    above, now character freezes after collision. Also all trivia logic is done as well, just need to populate with more questions

--- runner_class only.py
+++ runner_class only.py
@@ -1,6 +1,7 @@
 import pygame
 from sys import exit
 from random import randint, choice
+from copy import deepcopy
 
 class Player(pygame.sprite.Sprite):
 	def __init__(self):
@@ -56,7 +57,7 @@ class Obstacle(pygame.sprite.Sprite):
 
 
 def display_score():
-	current_time = int(pygame.time.get_ticks() / 1000) - start_time
+	current_time = int(pygame.time.get_ticks() / 1000) - start_time - total_pause
 	score_surf = test_font.render(f'Score: {current_time}',False,(64,64,64))
 	score_rect = score_surf.get_rect(center = (400,50))
 	screen.blit(score_surf,score_rect)
@@ -64,7 +65,7 @@ def display_score():
 
 def collision_sprite():
 	if pygame.sprite.spritecollide(player.sprite,obstacle_group,False):
-		obstacle_group.empty()
+		#obstacle_group.empty()
 		return False
 	else: return True
 
@@ -76,12 +77,20 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 magic_font_30 = pygame.font.Font('font/witches-magic.ttf', 30)
 magic_font_10 = pygame.font.Font('font/witches-magic.ttf', 10)
+
+freeze_screen = False
 game_active = False
 trivia_screen = False
 start_time = 0
+pause_start = 0
+pause_end = 0
+total_pause = 0
 score = 0
 bg_music = pygame.mixer.Sound('audio/music.wav')
 #bg_music.play(loops = -1)
+
+
+
 
 #Groups
 player = pygame.sprite.GroupSingle()
@@ -111,14 +120,20 @@ pygame.time.set_timer(obstacle_timer,1500)
 
 
 #load in trivia questions here
-trivia_questions = {
+TRIVIA_QUESTIONS_POOL = {
 	"Harry Potter is remembered as a legendary seeker at Hogwarts. How many snitches did Harry catch during his playing years?": ["a) 5 snitches", "b) 2 snitches", "c) 7 snitches", "d) 1 snitch",
 	pygame.K_c],
 	"Harry Potter has used his broom in masterful ways to get out of tight situations, which was Harry's first ever broom?": ["a) The Bongo 45", "b) The Nimbus 2000", "c) The Demented Horror", "d) The Firebolt",
 	pygame.K_b],
-}
+	"In his 7th year at Hogwarts, Harry famously ventured off to find Voldemort's Horcruxes, which object was not a Horcrux?" : ["a) Tom Riddle's Diary", "b) The Elder Wand", "c) The Diadem of Ravenclaw", "d) Marvolo Gaunt's Ring",
+																															  pygame.K_b],
+
+}  #we keep this as a constant, since every time we use a question we remove it from the dict in order to prevent repeating questions. When we run out of questions we reload all questions to prevent crash
+
+trivia_questions = deepcopy(TRIVIA_QUESTIONS_POOL)
  
-curr_trivia_question, curr_trivia_ans = choice(list(trivia_questions.items())) #refresh this after every key press
+curr_trivia_question, curr_trivia_ans = choice(list(trivia_questions.items())) #refresh this after every trivia question is answered to get a new one
+trivia_questions.pop(curr_trivia_question)
 
 
 
@@ -131,12 +146,32 @@ while True:
 		if game_active:
 			if event.type == obstacle_timer:
 				obstacle_group.add(Obstacle(choice(['fly'])))
-		elif trivia_screen:
+
+		elif freeze_screen:
 			if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+				trivia_screen = True
+				freeze_screen = False
+				obstacle_group.empty()
+
+		elif trivia_screen:
+			if event.type == pygame.KEYDOWN and event.key == correct_key:
 				game_active = True
 				trivia_screen = False
-				curr_trivia_question, curr_trivia_ans = choice(list(trivia_questions.items())) 
-			elif event.type == pygame.KEYDOWN:
+				if len(trivia_questions) == 0:
+					trivia_questions = deepcopy(TRIVIA_QUESTIONS_POOL)
+				else:
+					curr_trivia_question, curr_trivia_ans = choice(list(trivia_questions.items()))
+					trivia_questions.pop(curr_trivia_question)
+
+
+
+				#calculate the amount of time we spent waiting so the score is not crazy as shit
+				pause_end = pygame.time.get_ticks()
+				total_pause += int((pause_end - pause_start) / 1000)
+				pause_end = 0 #reset our timer variables
+				pause_start = 0
+
+			elif event.type == pygame.KEYDOWN and event.key in incorrect_keys:
 				game_active = False
 				trivia_screen = False
 		
@@ -144,6 +179,7 @@ while True:
 			if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
 				game_active = True
 				start_time = int(pygame.time.get_ticks() / 1000)
+				total_pause = 0
 
 
 	if game_active:
@@ -158,7 +194,25 @@ while True:
 
 		game_active = collision_sprite()
 		if not game_active:
-			trivia_screen = True
+
+			freeze_screen = True
+			pause_start = pygame.time.get_ticks()  #start the pause timer
+	
+	elif freeze_screen:
+		screen.blit(sky_surface,(0,0))
+		
+		player.draw(screen)
+		obstacle_group.draw(screen)
+
+		#game over text
+
+		dead_message = magic_font_30.render("PRESS ENTER TO TRY TO REVIVE!",True,(255,255,255))
+		dead_message_rect = dead_message.get_rect(center = (400,40))
+		screen.blit(dead_message, dead_message_rect)
+
+
+		
+
 	
 	elif trivia_screen:
 		screen.blit(trivia_surface, (0,0))
@@ -171,6 +225,25 @@ while True:
 		
 		screen.blit(trivia_message, trivia_message_rect)
 		screen.blit(trivia_question, trivia_question_rect)
+
+		trivia_answer_a = magic_font_10.render(curr_trivia_ans[0],True,(0,0,0))
+		trivia_answer_b = magic_font_10.render(curr_trivia_ans[1],True,(0,0,0))
+		trivia_answer_c = magic_font_10.render(curr_trivia_ans[2],True,(0,0,0))
+		trivia_answer_d = magic_font_10.render(curr_trivia_ans[3],True,(0,0,0))
+
+		trivia_answer_a_rect = trivia_answer_a.get_rect(center = (400, 150))
+		trivia_answer_b_rect = trivia_answer_b.get_rect(center = (400, 200))
+		trivia_answer_c_rect = trivia_answer_c.get_rect(center = (400, 250))
+		trivia_answer_d_rect = trivia_answer_d.get_rect(center = (400, 300))
+
+		screen.blit(trivia_answer_a, trivia_answer_a_rect)
+		screen.blit(trivia_answer_b, trivia_answer_b_rect)
+		screen.blit(trivia_answer_c, trivia_answer_c_rect)
+		screen.blit(trivia_answer_d, trivia_answer_d_rect)
+
+		possible_keys = [pygame.K_a, pygame.K_b, pygame.K_c, pygame.K_d]
+		correct_key = curr_trivia_ans[-1] #by convention I'm storing the correct key that the user should press as the last element of the list
+		incorrect_keys = [key for key in possible_keys if key != correct_key]
 	
 	
 		
